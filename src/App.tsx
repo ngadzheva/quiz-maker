4d7471fc72b8ef487d1ba452ca9697dd27063770
ngadzheva <nmgadzheva@gmail.com>
2024-04-10 00:05:51 +0300

    Fix Github pages build

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { Routes, Route} from "react-router-dom";
 import QuizResults from './components/QuizResults';
 import QuizMaker from './components/QuizMaker';
 
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <Routes>
+      <Route path='/' element={<QuizMaker />} />
+      <Route path='/results' element={<QuizResults />} />
+    </Routes>
   );
 }
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/'>
-      <Route path='' element={<QuizMaker />} />
-      <Route path='results' element={<QuizResults />} />
-    </Route>
-  )
-)
 
 export default App;
